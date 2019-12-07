d4cee4228f904fc033d0f51673a0defc811ed60d
EldarFaraday <eldarfara2000@gmail.com>
2019-12-07 14:27:30 +0300

    Ignore module_info.py for compatibility

--- SourceCode/module_info.py
+++ SourceCode/module_info.py
@@ -3,4 +3,4 @@
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
 export_dir = "D:/SteamLibrary/steamapps/common/MountBlade Warband/Modules/VictorianEraDev/"
-#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
+#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/" 
