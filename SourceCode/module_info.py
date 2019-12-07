7cae565e7d27d6264415f7c5b6942a201ae9ad3c
EldarFaraday <eldarfara2000@gmail.com>
2019-12-07 14:19:02 +0300

    Added source code files
    Initial.

---
+++ SourceCode/module_info.py
@@ -0,0 +1,6 @@
+
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+export_dir = "D:/SteamLibrary/steamapps/common/MountBlade Warband/Modules/VictorianEraDev/"
+#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
