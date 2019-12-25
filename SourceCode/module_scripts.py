e509bb0f6c345c000737f5cb15661efdc162817b
EldarFaraday <eldarfara2000@gmail.com>
2019-12-26 00:40:42 +0300

    Changes

--- SourceCode/module_scripts.py
+++ SourceCode/module_scripts.py
@@ -49510,7 +49510,7 @@ scripts = [
       (troop_add_merchandise_with_faction, ":cur_merchant", ":cur_faction", itp_type_arrows, 1),
       (troop_add_merchandise_with_faction, ":cur_merchant", ":cur_faction", itp_type_bolts, 2),
       (troop_add_merchandise_with_faction, ":cur_merchant", ":cur_faction", itp_type_bullets, 1),
-      (troop_add_merchandise_with_faction, ":cur_merchant", ":cur_faction", itp_type_pistols, 4),
+      (troop_add_merchandise_with_faction, ":cur_merchant", ":cur_faction", itp_type_pistol, 4),
       (troop_ensure_inventory_space, ":cur_merchant", merchant_inventory_space),
       (troop_sort_inventory, ":cur_merchant"), 
       (store_troop_gold, reg6, ":cur_merchant"),
